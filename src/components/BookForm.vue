6955f5c4f43c1b39350746ae5d129faec3fccffd
Jimmy022363 <48532827+Jimmy022363@users.noreply.github.com>
2019-06-03 15:22:26 -0500

    Add files via upload

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -2,7 +2,7 @@
   <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
     <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
-    
+        
     <div>
         <label for="finishedReading">
             Finished Reading
